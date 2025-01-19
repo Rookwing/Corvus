442ad9f167ca6b581972da34e005ab10ecd30d85
ptrockholt <ptrockholt@gmail.com>
2025-01-19 17:27:00 -0500

    Update Pipeline

--- Corvus/first_app.cpp
+++ Corvus/first_app.cpp
@@ -1,9 +1,36 @@
 #include"first_app.hpp"
 
+//std
+#include<stdexcept>
+
 namespace corvus {
+	FirstApp::FirstApp()
+	{
+	}
+	FirstApp::~FirstApp()
+	{
+	}
 	void FirstApp::run() {
 		while (!corvusWindow.shouldClose()) {
 			glfwPollEvents();
 		}
+	}
+
+	void FirstApp::createPipelineLayout() {
+		VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+		pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+		pipelineLayoutInfo.setLayoutCount = 0;
+		pipelineLayoutInfo.pSetLayouts = nullptr;
+		pipelineLayoutInfo.pushConstantRangeCount = 0;
+		pipelineLayoutInfo.pPushConstantRanges = nullptr;
+
+		if (vkCreatePipelineLayout(corvusDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS) {
+			throw std::runtime_error("Failed to create pipeline layout.");
+		}
+	}
+
+	void FirstApp::createPipeline() {
+		auto pipelineConfig = CorvusPipeline::defaultPipelineConfigInfo(corvusSwapChain.width(), corvusSwapChain.height());
+		pipelineConfig.renderPass = corvusSwapChain.getRenderPass();
 	}
 }
--- Corvus/shaders/simple_shader.vert
+++ Corvus/shaders/simple_shader.vert
@@ -1,6 +1,5 @@
 #version 460
-
-
+#extension GL_KHR_vulkan_glsl : enable
 vec2 positions[3] = vec2[] (
 	vec2(0.0,-0.5),
 	vec2(0.6,-0.1),
